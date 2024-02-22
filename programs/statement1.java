38d278f840cd31990093b7a8ea44d6afadb8872f
Selvakumar M <119500425+Selvahck14@users.noreply.github.com>
2024-02-22 14:29:53 +0530

    Add files via upload

---
+++ programs/statement1.java
@@ -0,0 +1,24 @@
+package selva;
+
+
+
+public class statement1 {
+
+	public static void main(String[] args) {
+		
+		int age =17;
+		
+		
+		
+		if(age>18)
+		{
+			System.out.println("Eligible to vote");
+		}
+		else
+		{
+			System.out.println("Not Eligible");
+		}
+
+	}
+
+}
